d1bb7e82b209c52897caafe06cf8279d327feb55
Grason Brennan <grason.brennan@libertyreach.com>
2022-09-15 09:30:33 -0400

    Break main into search() function, improve logging, allow multiple search directories, refactor arg names, add verbose setting.

--- src/Dingo/Dingo.py
+++ src/Dingo/Dingo.py
@@ -3,26 +3,75 @@ from termcolor import colored
 import argparse, os, sys
 import shutil
 import itertools
-
+import logging as log
 
 def intersection(lst1, lst2):
     return list(set(lst1) & set(lst2))
 
 
+def search(plugins_folder, source_folders, destination_folder, verbose = False)
+    try:
+        log.info("Locating dependencies, please wait...")
+        # Ensure the module path is an absolute path
+        plugins = [
+            os.path.join(plugins_folder, f)
+            for f in os.listdir(plugins_folder)
+            if os.path.isfile(os.path.join(plugins_folder, f)) and ".dll" in f
+        ]
+
+        dependencies = []
+        for plugin in plugins:
+            log.info("Finding dependencies for" + plugin)
+            # Parse the PE header for the module
+            header = ModuleHeader(plugin)
+            architecture = header.getArchitecture()
+            imports = header.listAllImports()
+            dependencies += imports
+            dependencies = StringUtils.uniqueCaseInsensitive(dependencies, sort=True)
+
+        # walk and search
+        copy_from = []
+        copy_to = []
+        for folder in source_folders:
+            log.info("Searching in" ,folder,"for dependencies.")
+            for (
+                root,
+                dirs,
+                files,
+            ) in os.walk(folder):
+                here = intersection(files, dependencies)
+                for item in here:
+                    if "Qt" not in item:
+                        copy_from.append(os.path.join(root, item))
+                        copy_to.append(os.path.join(destination_folder, item))
+            log.info("Copying into", destination_folder)
+        for src, dst in zip(copy_from, copy_to):
+            shutil.copy(src, dst)
+            log.info(src, "->", dst)
+
+    except RuntimeError as e:
+        log.error("Error: {}".format(e))
+        sys.exit(1)
+
 def main():
     # Our supported command-line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "folder", help="Plugins folder for which direct dependencies will be loaded"
+        "folder",
+        default="D:/plugins",
+        help="Plugins folder for which direct dependencies will be loaded, typically plugins folder",
     )
-
+    parser.add_argument("-v", dest="verbose", default=False,help="Print verbose output")
     parser.add_argument(
-        "--bin", default="all", help="Location of .dlls, typically 3rdparty folder"
+        "-s",
+        dest = "search",
+        default="D:/3rdparty",
+        help="Location of .dlls, typically 3rdparty folder",
     )
     parser.add_argument(
         "--dest",
         default="dependency",
-        help="Folder location to copy dependency .dlls into",
+        help="Folder location to copy dependency .dlls into, typically dependency",
     )
     # If no command-line arguments were supplied, display the help message and exit
     if len(sys.argv) < 2:
@@ -34,51 +83,21 @@ def main():
     if not args.dest or not args.bin or not args.folder:
         parser.print_help()
         sys.exit(0)
-    try:
-        print("Locating dependencies, please wait...")
-        # Ensure the module path is an absolute path
-        args.folder = os.path.abspath(args.folder)
-        args.dest = os.path.abspath(args.dest)
-        args.bin = os.path.abspath(args.bin)
-        sys.path.insert(0, args.dest)
-        sys.path.insert(0, args.dest + "/..")
-        plugins = [
-            os.path.join(args.folder, f)
-            for f in os.listdir(args.folder)
-            if os.path.isfile(os.path.join(args.folder, f)) and ".dll" in f
-        ]
-        dependencies = []
-        for plugin in plugins:
-            print("\tFinding dependencies for", plugin)
-            # Parse the PE header for the module
-            header = ModuleHeader(plugin)
-            architecture = header.getArchitecture()
-            imports = header.listAllImports()
-            dependencies += imports
-            dependencies = StringUtils.uniqueCaseInsensitive(dependencies, sort=True)
+    if args.verbose:
+        log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+        log.info("Verbose output.")
+    else:
+        log.basicConfig(format="%(levelname)s: %(message)s")
+    
+    args.folder = os.path.abspath(args.folder)
+    args.dest = os.path.abspath(args.dest)
+    args.bin = os.path.abspath(args.bin)
+    sys.path.insert(0, args.dest)
+    sys.path.insert(0, args.dest + "/..")
 
-        # walk and search
-        copy_from = []
-        copy_to = []
-        print("Searching in", args.bin, "for dependencies.")
-        for (
-            root,
-            dirs,
-            files,
-        ) in os.walk(args.bin):
-            here = intersection(files, dependencies)
-            for item in here:
-                if "Qt" not in item:
-                    copy_from.append(os.path.join(root, item))
-                    copy_to.append(os.path.join(args.dest, item))
-        print("Copying into", args.dest)
-        for src, dst in zip(copy_from, copy_to):
-            shutil.copy(src, dst)
-            print(src, "->", dst)
+    search(args.folder, args.search, args.dest, args.verbose)
 
-    except RuntimeError as e:
-        print("Error: {}".format(e))
-        sys.exit(1)
+
 
 
 if __name__ == "__main__":
